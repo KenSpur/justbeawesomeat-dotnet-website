3fd72421a8aa4f97ca25397c7bd861cad395d019
Ken Spur <Spur.ken@justbeawesome.net>
2020-06-16 11:00:17 +0200

    Apply IPageData interface

--- Application.Shared/ContactPage/ContactPageData.cs
+++ Application.Shared/ContactPage/ContactPageData.cs
@@ -3,7 +3,7 @@ using System.Text.Json.Serialization;
 
 namespace Application.Shared.ContactPage
 {
-    public class ContactPageData
+    public class ContactPageData : IPageData
     {
         [JsonPropertyName("info")]
         public ICollection<Info> Info { get; set; } = new List<Info>();
